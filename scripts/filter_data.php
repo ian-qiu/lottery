ff217c187c6a360abe0f52a73adfd83181988029
qiu.yiyi <qiu.yiyi@immomo.com>
2013-11-30 19:53:02 +0800

    重新设定奇偶期数

--- scripts/filter_data.php
+++ scripts/filter_data.php
@@ -1,7 +1,7 @@
 <?php
 @ini_set('display_errors', 'On');
 error_reporting(E_ALL);
-$dir = '/Users/ian/Downloads/';
+$dir = dirname(__FILE__) . '/';
 $file = $dir . '3000.txt';
 
 if(!is_file($file)){
@@ -10,6 +10,7 @@ if(!is_file($file)){
 
 $odd = array();
 $even = array();
+$total = array();
 
 $lines = file($file);
 foreach($lines as $line){
@@ -21,19 +22,22 @@ foreach($lines as $line){
     if(empty($item) || empty($code)){
         continue;
     }
-    $code = substr($code, 2);
-    $is_even = intval(substr($item,-1,1)) % 2 == 0;
-    if($is_even && !in_array($code,$even) && count($even) < 500){
-        $even[] = $code;
-    }else if(!in_array($code,$odd) && count($odd) < 500){
-        $odd[] = $code;
-    }
-    if(count($even) + count($odd) == 1000){
+    if(count($total) >= 900){
         break;
+    }
+    $code = substr($code, 2);
+    if(!in_array($code,$total)){
+        $total[] = $code;
+    }
+    $is_even = intval(substr($item,-1,1)) % 2 == 0;
+    if($is_even && !in_array($code,$even)){
+        $even[] = $code;
+    }else if(!in_array($code,$odd)){
+        $odd[] = $code;
     }
 }
 
-file_put_contents($dir . '奇数.txt',implode(PHP_EOL,$odd));
-echo "'" . implode("','", $odd) ."'". PHP_EOL;
-file_put_contents($dir . '偶数.txt',implode(PHP_EOL,$even));
-echo "'" . implode("','", $even) ."'". PHP_EOL;
+file_put_contents($dir . '奇数.txt',implode("\r\n",$odd));
+file_put_contents('param.txt',"'奇数期':['" . implode("','", $odd) ."']," . PHP_EOL);
+file_put_contents($dir . '偶数.txt',implode("\r\n",$even));
+file_put_contents('param.txt',"'偶数期':['" . implode("','", $even) ."']," . PHP_EOL,FILE_APPEND);
