13e549ea096e6bb1c5c16759bbf8ae249a52fd73
IanQiu <IanQiu@ian-pc>
2013-12-15 12:27:49 +0800

    最近三百期

--- model/lottery_util.php
+++ model/lottery_util.php
@@ -7,30 +7,42 @@
  */
 class LotteryUtil{
     
-    public function getRecentIssues($item_date,$num){
-        $dir = ROOT_DIR . 'log/issues/';
-        $file = $dir . $item_date . '.txt';
-        if(is_file($file)){
-            $codes = file_get_contents($file);
-            return explode(',',$codes);
-        }
-        if(!is_dir($dir)){
-            mkdir($dir,755);
-        }
-        $codes = $this->getCodesByDate($item_date, $num);
-        file_put_contents($file, implode(',',$codes));
-        return $codes;
+    public function calRecent300V1($item_date,$item_code,$num = 300){
+        $db = new LotteryDBHelper();
+        $sql = "select item_code from shishicai where item_date <'$item_date' order by item_date desc limit 300";
+        $data = $db->getAll($sql);
+        return $this->getRecent300($data, $item_code);
     }
     
-    private function getCodesByDate($item_date,$num){
+    public function calRecent300V2($item_date,$item_code,$num = 300){
+        $db = new LotteryDBHelper();
         list(,$issue) = explode('-',$item_date);
         $sql = sprintf('select * from shishicai where item_date < "%s" and item_date like "%%%s" order by item_date desc limit %d',$item_date,$issue,$num);
-        $db = new LotteryDBHelper();
         $data = $db->getAll($sql);
-        $ret = array();
+        return $this->getRecent300($data, $item_code);
+    }
+    
+    private function getRecent300($data,$item_code,$num = 300){
+        $t1 = '1';
+        $t2 = '1';
+        $t3 = '1';
+        $v1 = substr($item_code,0,3);
+        $v2 = substr($item_code,1,3);
+        $v3 = substr($item_code,2,3);
         foreach($data as $tmp){
-            $ret[] = $tmp['item_code'];
+            $tmp_v1 = substr($tmp['item_code'],0,3);
+            if($v1 === $tmp_v1){
+                $t1 = '2';
+            }
+            $tmp_v2 = substr($tmp['item_code'],1,3);
+            if($v2 === $tmp_v2){
+                $t2 = '2';
+            }
+            $tmp_v3 = substr($tmp['item_code'],2,3);
+            if($v3 === $tmp_v3){
+                $t3 = '2';
+            }
         }
-        return array_unique($ret);
+        return $t1.$t2.$t3;
     }
 }
---
+++ scripts/update_recent_300.php
@@ -0,0 +1,19 @@
+<?php
+include dirname(__FILE__) . '/config/global.php';
+
+include_once ROOT_DIR . 'db/lottery_db_helper.php';
+
+include_once ROOT_DIR . 'model/lottery_util.php';
+
+$util = new LotteryUtil();
+$db = new LotteryDBHelper();
+$sql = "select * from shishicai order by item_date desc limit 500;";
+$data = $db->getAll($sql);
+foreach ($data as $v){
+    $item_date = $v['item_date'];
+    $item_code = $v['item_code'];
+    $recent_300_v1 = $util->calRecent300V1($item_date, $item_code);
+    $recent_300_v2 = $util->calRecent300V2($item_date, $item_code);
+    $sql = sprintf("update shishicai set recent_300_v1='%s',recent_300_v2='%s' where item_date='%s'",$recent_300_v1,$recent_300_v2,$item_date);
+    echo $sql;
+}
