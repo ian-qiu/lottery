446de7a98756d31427a5fd5b034b860e34600753
qiu.yiyi <qiu.yiyi@immomo.com>
2014-01-23 11:18:22 +0800

    highcharts

--- model/lottery_util.php
+++ model/lottery_util.php
@@ -1,175 +1,240 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-class LotteryUtil{
-    
-    public function calRecent300V1($item_date,$item_code,$num = 300){
+
+class LotteryUtil {
+
+    public function calRecent300V1($item_date, $item_code, $num = 300) {
         $db = new LotteryDBHelper();
         $sql = "select item_code from shishicai where item_date <'$item_date' order by item_date desc limit 300";
         $data = $db->getAll($sql);
         return $this->getRecent300($data, $item_code);
     }
-    
-    public function calRecent300V2($item_date,$item_code,$num = 300){
+
+    public function calRecent300V2($item_date, $item_code, $num = 300) {
         $data = $this->getRecent300V2List($item_date, $num);
         return $this->getRecent300($data, $item_code);
     }
-    
-    public function calRecent300V3($item_date,$item_code,$num = 300){
+
+    public function calRecent300V3($item_date, $item_code, $num = 300) {
         include ROOT_DIR . 'config/data.php';
         $data = $this->getRecent300V2List($item_date, $num);
         $ret = '';
-        for($start = 0;$start < 3;$start++){
+        for ($start = 0; $start < 3; $start++) {
             $codes = array();
-            foreach ($data as $tmp){
-                $codes[] = substr($tmp['item_code'],$start,3);
+            foreach ($data as $tmp) {
+                $codes[] = substr($tmp['item_code'], $start, 3);
             }
             $codes = array_diff($odd_list, $codes);
-            if(in_array(substr($item_code,$start,3),$codes)){
+            if (in_array(substr($item_code, $start, 3), $codes)) {
                 $ret .= '1';
-            }else{
+            } else {
                 $ret .= '2';
             }
         }
         return $ret;
     }
-    
-    public function calHitV1($item_date,$item_code){
+
+    public function calHitV1($item_date, $item_code) {
         $total = array();
-        for($i=0;$i<10;$i++){
-            for($j=0;$j<10;$j++){
-                for($k=0;$k<10;$k++){
+        for ($i = 0; $i < 10; $i++) {
+            for ($j = 0; $j < 10; $j++) {
+                for ($k = 0; $k < 10; $k++) {
                     $total[] = strval($i) . strval($j) . strval($k);
                 }
             }
         }
         $list = $this->getRecent300V2List($item_date);
         $ret = array();
-        foreach($list as $tmp){
-            $ret[] = substr($tmp['item_code'],2,3);
+        foreach ($list as $tmp) {
+            $ret[] = substr($tmp['item_code'], 2, 3);
         }
-        $list1 = array_diff($total,$ret);
+        $list1 = array_diff($total, $ret);
         include ROOT_DIR . 'config/data.php';
         $end = array_intersect($odd_list, $list1);
-        $code = substr($item_code,2,3);
-        if(in_array($code,$end)){
+        $code = substr($item_code, 2, 3);
+        if (in_array($code, $end)) {
             return 1;
-        }else{
+        } else {
             return 0;
         }
     }
-    
-    public function getRecent300V2List($item_date,$num = 300){
+
+    public function getRecent300V2List($item_date, $num = 300) {
         $db = new LotteryDBHelper();
-        list(,$issue) = explode('-',$item_date);
-        $sql = sprintf('select * from shishicai where item_date < "%s" and item_date like "%%-%s" order by item_date desc limit %d',$item_date,$issue,$num);
+        list(, $issue) = explode('-', $item_date);
+        $sql = sprintf('select * from shishicai where item_date < "%s" and item_date like "%%-%s" order by item_date desc limit %d', $item_date, $issue, $num);
         $data = $db->getAll($sql);
         return $data;
     }
-    
-    private function getRecent300($data,$item_code,$num = 300){
+
+    private function getRecent300($data, $item_code, $num = 300) {
         $t1 = '1';
         $t2 = '1';
         $t3 = '1';
-        $v1 = substr($item_code,0,3);
-        $v2 = substr($item_code,1,3);
-        $v3 = substr($item_code,2,3);
-        foreach($data as $tmp){
-            $tmp_v1 = substr($tmp['item_code'],0,3);
-            if($v1 === $tmp_v1){
+        $v1 = substr($item_code, 0, 3);
+        $v2 = substr($item_code, 1, 3);
+        $v3 = substr($item_code, 2, 3);
+        foreach ($data as $tmp) {
+            $tmp_v1 = substr($tmp['item_code'], 0, 3);
+            if ($v1 === $tmp_v1) {
                 $t1 = '2';
             }
-            $tmp_v2 = substr($tmp['item_code'],1,3);
-            if($v2 === $tmp_v2){
+            $tmp_v2 = substr($tmp['item_code'], 1, 3);
+            if ($v2 === $tmp_v2) {
                 $t2 = '2';
             }
-            $tmp_v3 = substr($tmp['item_code'],2,3);
-            if($v3 === $tmp_v3){
+            $tmp_v3 = substr($tmp['item_code'], 2, 3);
+            if ($v3 === $tmp_v3) {
                 $t3 = '2';
             }
         }
-        return $t1.$t2.$t3;
+        return $t1 . $t2 . $t3;
     }
-    
-    public static function getTotalCodes(){
+
+    public static function getTotalCodes() {
         $codes = array();
-        for($i=0;$i<1000;$i++){
+        for ($i = 0; $i < 1000; $i++) {
             $codes[] = sprintf("%03s", $i);
         }
         return $codes;
     }
-    
-    public static function isStraightCode($code){
+
+    public static function isStraightCode($code) {
         $arr = str_split($code);
-        sort($arr,SORT_NUMERIC);
-        list($a,$b,$c) = $arr;
-        if($a < 8 && $a+1==$b &&$b+1==$c){
+        sort($arr, SORT_NUMERIC);
+        list($a, $b, $c) = $arr;
+        if ($a < 8 && $a + 1 == $b && $b + 1 == $c) {
             return true;
         }
-        if($a == 0 && ($b == 8 && $c == 9 || $b == 1 && $c == 9)){
+        if ($a == 0 && ($b == 8 && $c == 9 || $b == 1 && $c == 9)) {
             return true;
         }
         return false;
     }
-    
-    public static function calSumValue($code){
+
+    public static function calSumValue($code) {
         return array_sum(str_split($code)) % 10;
     }
-    
-    public static function calTrendCode($code){
+
+    public static function calTrendCode($code) {
         $arr = str_split($code);
         $ret = '';
-        foreach ($arr as $v){
-            if(in_array($v, array(0,3,6,9))){
+        foreach ($arr as $v) {
+            if (in_array($v, array(0, 3, 6, 9))) {
                 $ret .= '0';
-            }else if(in_array($v, array(1,4,7))){
+            } else if (in_array($v, array(1, 4, 7))) {
                 $ret .= '1';
-            }else if(in_array($v, array(2,5,8))){
+            } else if (in_array($v, array(2, 5, 8))) {
                 $ret .= '2';
             }
         }
         return $ret;
     }
-    
-    public static function getNextIssue($item_date){
-        list($date,$issue) = explode('-',$item_date);
-        if($issue == '120'){
-            $new_date = date('Ymd',strtotime('tomorrow',strtotime(substr($date,0,8)))) . '-001';
-        }else{
-            $new_date = $date . '-' . sprintf('%03d',intval($issue) + 1);
+
+    public static function getNextIssue($item_date) {
+        list($date, $issue) = explode('-', $item_date);
+        if ($issue == '120') {
+            $new_date = date('Ymd', strtotime('tomorrow', strtotime(substr($date, 0, 8)))) . '-001';
+        } else {
+            $new_date = $date . '-' . sprintf('%03d', intval($issue) + 1);
         }
         return $new_date;
     }
-	
-	public static function calDistribute($arr){
-		$length = count($arr);
-		$count = 0;
-		$ret = array();
-		for($i=0;$i<$length;$i++){
-			if(!isset($last)){
-				$last = $arr[$i];
-			}
-			if($arr[$i] == $last){
-				$count++;
-			}else{
-				$ret[] = array(
-					'value' => $last,
-					'count' => $count,
-				);
-				$count = 1;
-				$last = $arr[$i];
-			}
-			if($i + 1 == $length){
-				$ret[] = array(
-					'value' => $last,
-					'count' => $count,
-				);
-			}
-		}
-		return $ret;
-	}
+
+    public static function calDistribute($arr) {
+        $length = count($arr);
+        $count = 0;
+        $ret = array();
+        for ($i = 0; $i < $length; $i++) {
+            if (!isset($last)) {
+                $last = $arr[$i];
+            }
+            if ($arr[$i] == $last) {
+                $count++;
+            } else {
+                $ret[] = array(
+                    'value' => $last,
+                    'count' => $count,
+                );
+                $count = 1;
+                $last = $arr[$i];
+            }
+            if ($i + 1 == $length) {
+                $ret[] = array(
+                    'value' => $last,
+                    'count' => $count,
+                );
+            }
+        }
+        return $ret;
+    }
+    
+    public static function calMaxMiss($file,$calfunc,$settings = array()){
+        $min_count = isset($settings['min_count']) ? intval($settings['min_count']) : 0;
+        $show_items = isset($settings['show_items']) ? $settings['show_items'] : false;
+        $handle = fopen($file, 'r');
+        if(!$handle){
+            throw new Exception("Can not open $file");
+        }
+        $ret = array();
+        $items = array();
+        $count = 0;
+        while(true){
+            $line = fgets($handle,4096);
+            $line = trim($line);
+            if(empty($line)){
+                continue;
+            }
+            list($item_date,$item_code,$value) = call_user_func_array($calfunc,array($line));
+            if(!isset($last)){
+                $last = $value;
+            }
+            if($last === $value){
+                $count++;
+                $items[] = array(
+                    'item_code' => $item_code,
+                    'item_date' => $item_date
+                );
+            }else{
+                if($count > $min_count){
+                    $tmp = array(
+                        'value' => $value,
+                        'count' => $count,
+                    );
+                    if($show_items){
+                        $tmp['items'] = $items;
+                    }
+                    $ret[$count][] = $tmp;
+                }
+                $count = 1;
+                $last = $value;
+                $items = array();
+                $items[] = array(
+                    'item_code' => $item_code,
+                    'item_date' => $item_date
+                );
+            }
+            if(feof($handle)){
+                if($count > $min_count){
+                    $tmp = array(
+                        'value' => $value,
+                        'count' => $count,
+                    );
+                    if($show_items){
+                        $tmp['items'] = $items;
+                    }
+                    $ret[$count][] = $tmp;
+                }
+                break;
+            }
+        }
+        fclose($handle);
+        return $ret;
+    }
+
 }
